3f1606e3318af3e8fdeaf71631dba6095b5eacab
Eashan <eashanvgupta@gmail.com>
2025-08-23 16:54:34 -0500

    cleaned up code

--- vidtoascii.c
+++ vidtoascii.c
@@ -9,97 +9,71 @@
 #define SCALEX                   10
 #define SCALEY                   SCALEX*0.56
 
-// typedef struct {
-//     int width;
-//     int height;
-//     int channels;
-//     unsigned char *data;
-// } Image;
+typedef struct {
+    int width;
+    int height;
+    int channels;
+    unsigned char *data;
+} Image;
 
-// Image loadImage(const char *filename, int desiredChannels);
-// void convertToGrayscale(const Image *img, uint8_t *grayscaleData);
-// void computeAsciiArt(const uint8_t *grayscaleData, int width, int height, int scalex, int scaley, const char *grayRamp, char *characterData);
-// void printAsciiArt(const char *characterData, int outputCharWidth, int outputCharHeight);
+Image loadImage(const char *filename, int desiredChannels);
+void pixelsToGrayscale(const Image *img, uint8_t *grayscaleData);
+void computeAsciiArt(const uint8_t *grayscaleData, int width, int height, int scalex, int scaley, const char *grayRamp, char *characterData);
+void printAsciiArt(const char *characterData, int outputCharWidth, int outputCharHeight);
+void selectAsciiCharacter(const uint8_t averageDensity, const char *grayRamp, char *characterData, const int charIndex);
 
-void grayscaleCharacter(uint8_t averageDensity, char *grayRamp, char *characterData, size_t charIndex)
+Image loadImage(const char *filename, int desiredChannels)
+{
+   Image img;
+   img.data = stbi_load(filename, &img.width, &img.height, &img.channels, 0);
+   if (img.data != NULL) 
+   {
+       img.channels = desiredChannels; // Update channels to desired if loading was successful
+   }
+   return img;
+}
+
+void printAsciiArt(const char *characterData, int outputCharCountXAxis, int outputCharCountYAxis)
+{
+   // print out all the data in average density for testing
+   for(size_t y = 0; y < outputCharCountYAxis; y++)
+   {
+      for(size_t x = 0; x < outputCharCountXAxis; x++)
+      {
+         size_t index = x + (y * outputCharCountXAxis);
+         printf("%c", characterData[index]);
+      }
+      printf("\n");
+   }
+}
+void selectAsciiCharacter(const uint8_t averageDensity, const char *grayRamp, char *characterData, const int charIndex)
 {
    size_t rampSize = strlen(grayRamp);
    size_t index = (size_t) round((averageDensity / 255.0) * (rampSize - 1));
    characterData[charIndex] = grayRamp[index];
 }
 
-void pixelsToGrayscale(unsigned char *img, size_t totalImageBytes, int channels, uint8_t grayscaleData[])
+void pixelsToGrayscale(const Image *img, uint8_t *grayscaleData)
 {
-   for(unsigned char *pixelIndex = img; pixelIndex < (img + totalImageBytes); pixelIndex += channels)
+   for(unsigned char *pixelIndex = img->data; pixelIndex < (unsigned char*) (img->data + (img->width * img->height * img->channels)); pixelIndex += img->channels)
    {
       uint8_t R = *(pixelIndex);
       uint8_t G = *(pixelIndex + 1);
       uint8_t B = *(pixelIndex + 2);
       uint8_t GrayScale = (uint8_t) round(0.21*R + 0.72*G + 0.07*B);
-      grayscaleData[(pixelIndex - img) / channels] = GrayScale;
+      grayscaleData[(pixelIndex - img->data) / img->channels] = GrayScale;
    }
 }
 
-
-
-int main(void)
+void computeAsciiArt(const uint8_t *grayscaleData, int width, int height, int outputCharCountXAxis, int outputCharCountYAxis, const char *grayRamp, char *characterData)
 {
-   
-   // channels - number of bytes in each pixel
-   // width - width of the image
-   // height - height of the image
-   int width, height, channels;
-   char *imageName = "IMG_9444.jpg";
-   int desiredChannels = 3;
+   for(size_t blocky = 0; blocky < outputCharCountYAxis; blocky++) {
+      size_t startY = blocky * height / outputCharCountYAxis;
+      size_t endY   = (blocky+1) * height / outputCharCountYAxis;
 
-   unsigned char *img = stbi_load(imageName, &width, &height, &channels, desiredChannels);
-   if(img == NULL)
-   {
-      printf("Error in loading image\n");
-      return 1;
-   }
-
-   // number of horizontal characters to output
-   // same as the number of blocks the image wants to be broken into, since each block represents a char
-   size_t outputCharXct = width/SCALEX;
-   // number of vertical characters to output
-   size_t outputCharYct = height/SCALEY;
-
-   printf("Loaded image with width of %dpx, height of %dpx, and %d channels\n", width, height, channels);
-   
-   size_t grayscaleDataBuffer = width * height;
-   uint8_t *grayscaleData = malloc(grayscaleDataBuffer * sizeof(uint8_t));
-   if(!grayscaleData)
-   {
-      printf("Error allocating memory for grayscale data\n");
-      stbi_image_free(img);
-      return 1;
-   }
-
-   // for black background
-   //char *grayRamp = " .:~+*#$";
-
-   // for white background
-   char *grayRamp = "#$*+~:. ";
-   size_t rampSize = strlen(grayRamp);
-
-   char *characterData = (char *)malloc(outputCharXct * outputCharYct * sizeof(char));
-   char *pchar = characterData;
-
-   // convert the RGB data into grayscale data and then store into 1D array.
-   // channels - number of bytes in each pixel
-   // iterating over channels (p += channels) lets us jump to the next pixel
-   // iterating upto (img + totalImageBytes) lets us iterate upto the memory address of the last pixel in the image
-   size_t totalImageBytes = (size_t) (width * height * channels);
-   pixelsToGrayscale(img, totalImageBytes, channels, grayscaleData);
-
-   for(size_t blocky = 0; blocky < outputCharYct; blocky++) {
-      size_t startY = blocky * height / outputCharYct;
-      size_t endY   = (blocky+1) * height / outputCharYct;
-
-      for(size_t blockx = 0; blockx < outputCharXct; blockx++) {
-         size_t startX = blockx * width / outputCharXct;
-         size_t endX   = (blockx+1) * width / outputCharXct;
+      for(size_t blockx = 0; blockx < outputCharCountXAxis; blockx++) {
+         size_t startX = blockx * width / outputCharCountXAxis;
+         size_t endX   = (blockx+1) * width / outputCharCountXAxis;
 
          uint32_t currGridTotal = 0;
          size_t pixelcount = 0;
@@ -116,27 +90,59 @@ int main(void)
             averageDensity = (uint8_t)(currGridTotal / pixelcount);
          }
          
-         grayscaleCharacter(averageDensity, grayRamp, characterData, blocky * outputCharXct + blockx);
-         //printf("%i\n", averageDensity);
+         selectAsciiCharacter(averageDensity, grayRamp, characterData, blocky * outputCharCountXAxis + blockx);
       }
    }
-   // print out all the data in average density for testing
-   for(size_t y = 0; y < outputCharYct; y++)
+}
+
+int main(void)
+{
+   
+   // channels - number of bytes in each pixel  
+   // width - width of the image
+   // height - height of the image
+   const char *imgName = "media/michcar.jpg";
+   int desiredChannels = 3;
+   Image img = loadImage(imgName, desiredChannels);
+   if(img.data == NULL)
    {
-      for(size_t x = 0; x < outputCharXct; x++)
-      {
-         size_t index = x + (y * outputCharXct);
-         printf("%c", characterData[index]);
-      }
-      printf("\n");
+      printf("Error in loading image\n");
+      return 1;
    }
 
-   //print the char data to terminal
-   // for(size_t i = 0; i < width*height; i++)
-   // {
-   //    printf("%d\n", grayscaleData[i]);
-   // }
+   int outputCharCountXAxis = img.width/SCALEX;
+   int outputCharCountYAxis = img.height/SCALEY;
+
+   uint8_t *grayscaleData = (uint8_t*) malloc(img.width * img.height * sizeof(uint8_t));
+   char *characterData = malloc(outputCharCountXAxis * outputCharCountYAxis * sizeof(char));
+
+   if(grayscaleData == NULL || characterData == NULL)
+   {
+      free(grayscaleData);
+      free(characterData);
+      printf("Error allocating memory on the heap\n");
+      return 1;
+   }
+
+   // for black background
+   const char *grayRamp = " .:~+*#$";
+
+   // for white background
+   //char *grayRamp = "#$*+~:. ";
+
+   // processing
+   pixelsToGrayscale(&img, grayscaleData);
+   computeAsciiArt(grayscaleData, img.width, img.height, outputCharCountXAxis, outputCharCountYAxis, grayRamp, characterData);
+
+   // output
+   printAsciiArt(characterData, outputCharCountXAxis, outputCharCountYAxis);
+
+   //cleanup
+   free(grayscaleData);
+   free(characterData);
+   stbi_image_free(img.data);
    return 0;
+
 }
 
 // write a function to print hello world
